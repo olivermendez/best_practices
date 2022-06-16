06dce693d029e5aa49dcdf67e1e5a603ba875014
Oliver Yunior Mendez <39278672+olivermendez@users.noreply.github.com>
2022-06-16 17:51:31 -0400

    updates

--- models/sportbicycles.dart
+++ models/sportbicycles.dart
@@ -1,1 +1,19 @@
 import 'bicycles.dart';
+
+class SportBicycle extends Bicycle {
+  SportBicycle(
+    super.brand,
+    super.model,
+    super.color,
+    super.wheelCount,
+    super.saddleColor,
+    this.baseMaterial,
+  );
+
+  String baseMaterial;
+
+  @override
+  void paint(String colorName) {
+    super.model = colorName;
+  }
+}
